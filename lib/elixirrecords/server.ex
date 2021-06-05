47b00d98adb6ad436efd36118f9b80d621f9fa0d
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-06-05 19:29:30 +0200

    pagina para feedback al usuario

--- lib/elixirrecords/server.ex
+++ lib/elixirrecords/server.ex
@@ -35,15 +35,15 @@ defmodule Elixirrecords.Server do
 
         # Mandar la Tx
 
-        {:reply, true, state}
+        {:reply, {:ok, "Transacción realizada"}, state}
       
       else 
-        {:reply, "Usuario no encontrado", state}
+        {:reply, {:error, "Usuario no encontrado"}, state}
 
       end
 
     else 
-      {:reply, false, state}
+      {:reply, {:wait, false}, state}
 
     end
   
--- lib/elixirrecords_web/controllers/registro_controller.ex
+++ lib/elixirrecords_web/controllers/registro_controller.ex
@@ -7,11 +7,18 @@ defmodule ElixirrecordsWeb.RegistroController do
   def registro(conn, params) do
 
     res = Server.sendTx(params["username"])
-    IO.puts("La respuesta es: #{inspect res}")
-
+    case res do
+      {:error, msg} ->
+        conn
+        |> put_flash(:error, msg)
+        |> render("registro.html")
+      {:wait, msg} ->
+        render(conn, "registro.html")
+      {:ok, msg} ->
+        render(conn, "registrado.html")
+    end
     # Llamar al backend (el back llama a la base de datos)
     # El backend firma y envia
     # Una vez finiquitao el front-end (este de aqui) renderiza la pagina de que todo guay :D
-    render(conn, "registro.html")
   end
 end
