90ec661b7c4bc7890f6f881eadacb9229ecb3998
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-06-08 18:57:50 +0200

    smart contract modificado

--- priv/solidity/ContratoAsistencias.sol
+++ priv/solidity/ContratoAsistencias.sol
@@ -3,8 +3,22 @@ pragma solidity >=0.7.0 <0.9.0;
 contract ContratoAsistencias {
   event Asistencia(string correo, string fecha);
 
+  string [] public correos;
+  mapping(string => string) public listaAsistencias;
+
   function registrarAsistencia(string memory correo, string memory fecha) public {
+
+    correos.push(correo);
+    listaAsistencias[correo] = fecha;
+
     emit Asistencia(correo, fecha);
   }
 
+  function getCorreos() public view returns (string [] memory) {
+    return correos;
+  }
+
+  function getFecha(string memory correo) public view returns (string memory){
+    return listaAsistencias[correo];
+  }
 }
